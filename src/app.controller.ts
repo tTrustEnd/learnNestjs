7d7c7d18bcefad79bfd731d3b851ce98147e7814
Truong <truong.nq185728@gmail.com>
2023-07-03 07:26:20 +0700

    ejs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
@@ -9,4 +9,5 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
 }
