a7a5f1c53ba2ce02dc76937f38ba5c05d5073f67
Truong <truong.nq185728@gmail.com>
2023-07-07 16:05:42 +0700

    file config

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, ParseFilePipeBuilder, HttpStatus } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { CreateFileDto } from './dto/create-file.dto';
 import { UpdateFileDto } from './dto/update-file.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
-
-  @Post()
-  create(@Body() createFileDto: CreateFileDto) {
-    return this.filesService.create(createFileDto);
+//https://docs.nestjs.com/techniques/file-upload
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile(
+    new ParseFilePipeBuilder()
+    .addFileTypeValidator({
+      fileType: /(jpg|jpeg|png|image\/png)$/ ,
+    })
+    .addMaxSizeValidator({
+      maxSize: 10000*1024
+    })
+    .build({
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
+    }),
+  ) file: Express.Multer.File) {
+    console.log(file);
   }
 
   @Get()
--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { MulterConfigService } from './files.config';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports:[MulterModule.registerAsync({
+    useClass: MulterConfigService,
+  })],
   controllers: [FilesController],
   providers: [FilesService]
 })
